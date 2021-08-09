990443055983a1d98eb785b93d498bddb620b29d
Brice Carter <bisforbrice@gmail.com>
2021-08-09 12:28:57 -0500

    updated pollInterval to remove comment lag

--- frontend/src/views/case/case.js
+++ frontend/src/views/case/case.js
@@ -71,7 +71,7 @@ function Case () {
         }
       }`
 
-  const {loading, error, data} = useQuery(GET_CASE, {variables: {caseid}, fetchPolicy: "network-only"});
+  const {loading, error, data} = useQuery(GET_CASE, {variables: {caseid}, fetchPolicy: "network-only", pollInterval: 500});
 
   if (error) return <p> {error.message} </p>
   if (loading) return <p> loading... </p>
