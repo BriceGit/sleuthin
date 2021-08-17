8c7205444839041b2f45ca574f54bd46ddb46e4c
BriceGit <bisforbrice@gmail.com>
2021-08-17 09:00:42 -0700

    for testing

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 
 require('dotenv').config();
 
